2ab089c2ca2751cab83d6eca98d3b1196ad032f7
504743342 <504743342@qq.com>
2023-03-16 15:22:10 +0800

    删掉 FormItem.vue 的 Watch 和 去掉 placeholder 的 this

--- src/components/Money/FormItem.vue
+++ src/components/Money/FormItem.vue
@@ -5,24 +5,23 @@
       <input type="text" 
             :value="value"
              @input="onValueChanged($event.target.value)"
-             :placeholder="this.placeholder" />
+             :placeholder="placeholder" />
     </label>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-import {Component, Watch, Prop} from 'vue-property-decorator'
-@Component
-export default class FormItem extends Vue{
-  @Prop({default: ''}) readonly value!: string
-  @Prop({required: true}) fieldName!: string
-  @Prop() placeholder?: string
-  @Watch('value')
-  onValueChanged(value: string) {
-    this.$emit('update:value', value)
-  }
-};
+  import Vue from 'vue'
+  import {Component, Watch, Prop} from 'vue-property-decorator'
+  @Component
+  export default class FormItem extends Vue{
+    @Prop({default: ''}) readonly value!: string
+    @Prop({required: true}) fieldName!: string
+    @Prop() placeholder?: string
+    onValueChanged(value: string) {
+      this.$emit('update:value', value)
+    }
+  };
 </script>
 
 <style lang="scss" scoped>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
     <Layout class-prefix="layout">
-      
         <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
         <Types :value.sync="record.type"/>
         <div class="notes">
