a71cf80f1a2a28d558de8c082043e3bc8c0d2e8e
504743342 <504743342@qq.com>
2023-03-22 21:30:34 +0800

    Labels.vue 使用 Vuex

---
+++ src/mixins/TagHelper.ts
@@ -0,0 +1,16 @@
+// mixins.js
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+// You can declare mixins as the same style as components.
+@Component
+export class TagHelper extends Vue {
+  /* eslint-disable*/
+  createTag() {
+    const name = window.prompt('请输入标签名');
+    if (!name) { return window.prompt('标签名不能为空'); }
+    this.$store.commit('createTag', name);
+  }
+}
+
+export default TagHelper;
--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,65 +1,68 @@
 <template>
-    <div>
-        <Layout>
-            <div class="tags">
-                <router-link class="tag" 
-                            v-for="tag in tags" :key="tag.id"
-                            :to="`/labels/edit/${tag.id}`">
-                    <span>{{tag.name}}</span>
-                    <Icon name="right"/>
-                </router-link>
-            </div>
-            <div class="createTag-wrapper">
-                <Button @click="createTag">新建标签</Button>
-            </div>
-        </Layout>
-    </div>
+  <div>
+    <Layout>
+      <div class="tags">
+        <router-link class="tag"
+                     v-for="tag in tags" :key="tag.id"
+                     :to="`/labels/edit/${tag.id}`">
+          <span>{{ tag.name }}</span>
+          <Icon name="right"/>
+        </router-link>
+      </div>
+      <div class="createTag-wrapper">
+        <Button @click="createTag">新建标签</Button>
+      </div>
+    </Layout>
+  </div>
 </template>
 
 <script lang="ts">
-    import Vue from 'vue'
-    import {Component} from 'vue-property-decorator';
-    import Button from '@/components/Button.vue'
+import {Component} from 'vue-property-decorator';
+import Button from '@/components/Button.vue';
+import TagHelper from '@/mixins/TagHelper';
+import {mixins} from 'vue-class-component';
 
-    @Component({
-        components: {Button}
-    })
-    export default class Labels extends Vue{
-        // TODO
-        tags = []
-        
-        createTag(){
-            const name = window.prompt('请输出标签名')
-            if(name) {
-                //TODO
-            //  store.createTag(name)
-            }
-        }
+@Component({
+  components: {Button},
+  computed: {
+    tags() {
+      return this.$store.state.tagList;
     }
+  }
+})
+export default class Labels extends mixins(TagHelper) {
+  /* eslint-disable*/
+  beforeCreate() {
+    this.$store.commit('fetchTags')
+  }
+}
 </script>
 
 <style lang="scss" scoped>
-    .tags {
-        background: white;
-        font-size: 16px;
-        padding-left: 16px;
-        > .tag {
-            min-height: 44px;
-            display: flex;
-            align-items: center;
-            justify-content: space-between;
-            border-bottom: 1px solid #e6e6e6;
-            svg {
-                width: 18px;
-                height: 18px;
-                color: #666;
-                margin-right: 16px;
-            }
-        }
+.tags {
+  background: white;
+  font-size: 16px;
+  padding-left: 16px;
+
+  > .tag {
+    min-height: 44px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    border-bottom: 1px solid #e6e6e6;
+
+    svg {
+      width: 18px;
+      height: 18px;
+      color: #666;
+      margin-right: 16px;
     }
-     .createTag-wrapper {
-            text-align: center;
-            padding: 16px;
-            margin-top: 44-16px;
-        }
+  }
+}
+
+.createTag-wrapper {
+  text-align: center;
+  padding: 16px;
+  margin-top: 44-16px;
+}
 </style>
