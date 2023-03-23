4edcf9b4e3b1ce892e38a640f863b17afc28b827
504743342 <504743342@qq.com>
2023-03-23 18:49:46 +0800

    修复刷新变成 404 的 bug

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -1,90 +1,87 @@
 <template>
-    <Layout>
-        <div class="navBar">
-            <Icon class="leftIcon" name="left" @click="goBack"/>
-            <span class="title">编辑标签</span>
-            <span class="rightIcon" />
-        </div>
-        <div class="form-wrapper">
-            <FormItem :value="tag.name" 
-                    @update:value="update"
-                    field-name="标签名" placeholder="请输入标签名"/>
-        </div>
-        <div class="button-wrapper">
-            <Button @click="remove">删除标签</Button>
-        </div>
-    </Layout>
+  <Layout>
+    <div class="navBar">
+      <Icon class="leftIcon" name="left" @click="goBack" />
+      <span class="title">编辑标签</span>
+      <span class="rightIcon" />
+    </div>
+    <div class="form-wrapper">
+      <FormItem
+        :value="tag.name"
+        @update:value="update"
+        field-name="标签名"
+        placeholder="请输入标签名"
+      />
+    </div>
+    <div class="button-wrapper">
+      <Button @click="remove">删除标签</Button>
+    </div>
+  </Layout>
 </template>
 
 <script lang="ts">
-    import Vue from 'vue'
-    import {Component} from 'vue-property-decorator'
-    import FormItem from '@/components/Money/FormItem.vue'
-    import Button from '@/components/Button.vue'
-    
-    @Component({
-        components:{FormItem, Button},
-    })
-    export default class EditLabel extends Vue{
-        get tag() {
-            return this.$store.state.currentTag
-        }
+import Vue from 'vue'
+import {Component} from 'vue-property-decorator'
+import FormItem from '@/components/Money/FormItem.vue'
+import Button from '@/components/Button.vue'
 
-        created() {
-            const id = this.$route.params.id
-            this.$store.commit('setCurrentTag', id)
-            if(!this.tag){
-                this.$router.replace('/404')
-            }
-        }
-        update(name: string) {
-            if(this.tag){
-                // TODO
-                // store.updateTag(this.tag.id, name)
-            }
-        }
-        remove(){
-            if(this.tag) {
-                // TODO
-            //  if(store.removeTag(this.tag.id)){
-            //     this.$router.back()
-            //  } else{
-            //     window.alert('删除失败')
-            //  }
-            return
-            }
-        }
-        goBack() {
-            this.$router.back()
+@Component({
+    components:{FormItem, Button},
+})
+export default class EditLabel extends Vue{
+    get tag() {
+        return this.$store.state.currentTag
+    }
+
+    created() {
+        const id = this.$route.params.id
+        this.$store.commit('fetchTags')
+        this.$store.commit('setCurrentTag', id)
+        if(!this.tag){
+            this.$router.replace('/404')
         }
     }
+    update(name: string) {
+        if(this.tag){
+            this.$store.commit('updateTag', {id: this.tag.id, name})
+        }
+    }
+    remove(){
+        if(this.tag) {
+            this.$store.commit('removeTag', this.tag.id)
+        }
+    }
+    goBack() {
+        this.$router.back()
+    }
+}
 </script>
 
 <style lang="scss" scoped>
-    .navBar {
-        text-align: center;
-        font-size: 16px;
-        padding: 12px 16px;
-        background: white;
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        > .leftIcon{
-           width: 24px;
-           height: 24px;
-        }
-        > .rightIcon {
-            width: 24px;
-            height: 24px;
-        }
-    }
-    .form-wrapper {
-        background: white;
-        margin-top: 8px;
-    }
-    .button-wrapper {
-        text-align: center;
-        padding: 16px;
-        margin-top: 44-16px;
-    }
+.navBar {
+  text-align: center;
+  font-size: 16px;
+  padding: 12px 16px;
+  background: white;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  > .leftIcon {
+    width: 24px;
+    height: 24px;
+  }
+  > .rightIcon {
+    width: 24px;
+    height: 24px;
+  }
+}
+.form-wrapper {
+  background: white;
+  margin-top: 8px;
+}
+.button-wrapper {
+  text-align: center;
+  padding: 16px;
+  margin-top: 44-16px;
+}
 </style>
