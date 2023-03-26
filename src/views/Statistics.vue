a5af3f1acdc1b04b0c81cb5d1d151e7b5f21db82
504743342 <504743342@qq.com>
2023-03-26 19:15:57 +0800

    用列表展示数据 并且添加相应的 css

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -10,11 +10,18 @@
       :data-source="intervalList"
       :value.sync="interval"
     />
-    <div>
-      type: {{ type }}
-      <br />
-      interval: {{ interval }}
-    </div>
+    <ol>
+      <li v-for="(group, index) in result" :key="index">
+        <h3 class="title">{{ group.title }}</h3>
+        <ol>
+          <li v-for="item in group.items" :key="item.id" class="record">
+            <span>{{tagString(item.tags)}}</span>
+            <span class="notes">{{item.notes}}</span>
+            <span>￥{{item.amount}}</span>
+          </li>
+        </ol>
+      </li>
+    </ol>
   </Layout>
 </template>
 
@@ -29,6 +36,31 @@ import recordTypeList from "@/constants/recordTypeList";
   components: { Tabs },
 })
 export default class Statistics extends Vue {
+  tagString(tags: Tag[]) {
+    return tags.length === 0 ? '无' : tags.join(',')
+  }
+  get recordList() {
+    return (this.$store.state as RootState).recordList;
+  }
+
+  get result() {
+    const { recordList } = this;
+    type HashTableValue = { title: string; items: RecordItem[] };
+
+    const hashTable: { [key: string]: HashTableValue } = {};
+    for (let i = 0; i < recordList.length; i++) {
+      const [date, time] = recordList[i].createAt!.split("T");
+      hashTable[date] = hashTable[date] || { title: date, items: [] };
+      hashTable[date].items.push(recordList[i]);
+      console.log(hashTable);
+    }
+    return hashTable;
+  }
+
+  beforeCreate() {
+    this.$store.commit("fetchRecords");
+  }
+
   type = "-";
   interval = "day";
   intervalList = intervalList;
@@ -37,6 +69,26 @@ export default class Statistics extends Vue {
 </script>
 
 <style lang="scss" scoped>
+%item {
+  padding: 8px 16px;
+  line-height: 40px;
+  display: flex;
+  justify-content: space-between;
+  align-content: center;
+}
+.title {
+  @extend %item
+}
+.record {
+  background: white;
+  @extend %item
+}
+.notes {
+  margin-right: auto;
+  margin-left: 8px;
+  color: #999;
+}
+
 ::v-deep {
   .type-tabs-item {
     background: white;
