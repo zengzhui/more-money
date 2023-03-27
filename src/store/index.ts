3747a7d92dd20750d09906cf4d38bb40aa1c091f
504743342 <504743342@qq.com>
2023-03-27 11:43:54 +0800

    修复一进页面就弹出四个提示的 bug

--- src/store/index.ts
+++ src/store/index.ts
@@ -40,12 +40,12 @@ const store = new Vuex.Store({
           break;
         }
       }
-      if(index >= 0){
+      if (index >= 0) {
         state.tagList.splice(index, 1);
         store.commit("saveTags");
-        router.back()
-      }else {
-        window.alert('删除失败')
+        router.back();
+      } else {
+        window.alert("删除失败");
       }
     },
     fetchRecords(state) {
@@ -70,18 +70,23 @@ const store = new Vuex.Store({
       state.tagList = JSON.parse(
         window.localStorage.getItem("tagList") || "[]"
       );
+      if (!state.tagList || state.tagList.length === 0) {
+        store.commit("createTag", "衣");
+        store.commit("createTag", "食");
+        store.commit("createTag", "住");
+        store.commit("createTag", "行");
+      }
     },
     createTag(state, name: string) {
       state.createTagError = null;
       const names = state.tagList.map((item) => item.name);
       if (names.indexOf(name) >= 0) {
-        state.createTagError = new Error('tag name duplicated')
-        return
+        state.createTagError = new Error("tag name duplicated");
+        return;
       }
       const id = createId().toString();
       state.tagList.push({ id, name: name });
       store.commit("saveTags");
-      window.alert("添加成功");
     },
     saveTags(state) {
       window.localStorage.setItem("tagList", JSON.stringify(state.tagList));
