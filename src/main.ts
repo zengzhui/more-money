8fe39bc7db78881dc3686073ab1925fdef0ca042
504743342 <504743342@qq.com>
2023-03-29 16:24:42 +0800

    修复二维码部署到网站不显示的问题

--- src/main.ts
+++ src/main.ts
@@ -24,7 +24,7 @@ new Vue({
 if (document.documentElement.clientWidth > 500) {
   window.alert('请使用手机打开本页面,以保证浏览效果')
   const img = document.createElement("img");
-  img.src = "/qrcode.png";
+  img.src = "./qrcode.png";
   img.style.position = "fixed";
   img.style.left = "50%";
   img.style.top = "50%";
