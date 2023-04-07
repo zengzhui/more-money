f5b5562023330949606086848d9003ffd08c8079
zzh <10003482+zengzhaohui@user.noreply.gitee.com>
2023-04-07 10:19:55 +0800

    添加 createdAt 输入框

--- src/components/Money/FormItem.vue
+++ src/components/Money/FormItem.vue
@@ -1,27 +1,43 @@
 <template>
   <div>
     <label class="formItem">
-      <span class="name">{{this.fieldName}}</span>
-      <input type="text" 
-            :value="value"
-             @input="onValueChanged($event.target.value)"
-             :placeholder="placeholder" />
+      <span class="name">{{ this.fieldName }}</span>
+      <template v-if="type === 'date'">
+        <input :type="type || 'text'"
+               :value="x(value)"
+               @input="onValueChanged($event.target.value)"
+               :placeholder="placeholder"/>
+      </template>
+      <template v-else>
+        <input :type="type || 'text'"
+               :value="value"
+               @input="onValueChanged($event.target.value)"
+               :placeholder="placeholder"/>
+      </template>
+
     </label>
   </div>
 </template>
 
 <script lang="ts">
-  import Vue from 'vue'
-  import {Component, Prop} from 'vue-property-decorator'
-  @Component
-  export default class FormItem extends Vue{
-    @Prop({default: ''}) readonly value!: string
-    @Prop({required: true}) fieldName!: string
-    @Prop() placeholder?: string
-    onValueChanged(value: string) {
-      this.$emit('update:value', value)
-    }
+import Vue from 'vue';
+import {Component, Prop} from 'vue-property-decorator';
+import dayjs from 'dayjs';
+
+@Component
+export default class FormItem extends Vue {
+  @Prop({default: ''}) readonly value!: string;
+  @Prop({required: true}) fieldName!: string;
+  @Prop() placeholder?: string;
+  @Prop() type?: string;
+
+  onValueChanged(value: string) {
+    this.$emit('update:value', value);
   }
+  x(isoString: string) {
+    return dayjs(isoString).format('YYYY-MM-DD')
+  }
+}
 </script>
 
 <style lang="scss" scoped>
@@ -30,9 +46,11 @@
   padding-left: 16px;
   display: flex;
   align-items: center;
+
   .name {
     padding-right: 16px;
   }
+
   input {
     height: 40px;
     flex-grow: 1;
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,8 +1,16 @@
 <template>
   <div>
     <Layout class-prefix="layout">
-      <NumberPad :value.sync="record.amount" @submit="saveRecord" />
-      <Tabs :data-source="recordTypeList" :value.sync="record.type" />
+      <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
+      <Tabs :data-source="recordTypeList" :value.sync="record.type"/>
+      <div class="createdAt">
+        <FormItem
+          field-name="日期"
+          type="date"
+          placeholder="在这里输入日期"
+          :value.sync="record.createdAt"
+        />
+      </div>
       <div class="notes">
         <FormItem
           field-name="备注"
@@ -10,22 +18,22 @@
           :value.sync="record.notes"
         />
       </div>
-      <Tags @update:value="record.tags = $event" />
+      <Tags @update:value="record.tags = $event"/>
     </Layout>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-import Tags from "@/components/Money/Tags.vue";
-import FormItem from "@/components/Money/FormItem.vue";
-import NumberPad from "@/components/Money/NumberPad.vue";
-import { Component } from "vue-property-decorator";
-import recordTypeList from "@/constants/recordTypeList";
-import Tabs from "@/components/Tabs.vue";
+import Vue from 'vue';
+import Tags from '@/components/Money/Tags.vue';
+import FormItem from '@/components/Money/FormItem.vue';
+import NumberPad from '@/components/Money/NumberPad.vue';
+import {Component} from 'vue-property-decorator';
+import recordTypeList from '@/constants/recordTypeList';
+import Tabs from '@/components/Tabs.vue';
 
 @Component({
-  components: { Tags, FormItem, Tabs, NumberPad },
+  components: {Tags, FormItem, Tabs, NumberPad},
 })
 export default class Money extends Vue {
   get recordList() {
@@ -34,21 +42,30 @@ export default class Money extends Vue {
 
   recordTypeList = recordTypeList;
 
-  record: RecordItem = { tags: [], notes: "", type: "-", amount: 0 };
+  record: RecordItem = {
+    tags: [],
+    notes: '',
+    type: '-',
+    amount: 0,
+    createdAt: new Date().toISOString()
+  };
+
   created() {
-    this.$store.commit("fetchRecords");
+    this.$store.commit('fetchRecords');
   }
+
   onUpdateNotes(value: string) {
     this.record.notes = value;
   }
+
   saveRecord() {
     if (!this.record.tags || this.record.tags.length === 0) {
-      return window.alert("请至少选择一个标签");
+      return window.alert('请至少选择一个标签');
     }
-    this.$store.commit("createRecord", this.record);
+    this.$store.commit('createRecord', this.record);
     if (this.$store.state.createRecordError === null) {
-      window.alert("已保存");
-      this.record.notes = "";
+      window.alert('已保存');
+      this.record.notes = '';
     }
   }
 }
@@ -59,6 +76,7 @@ export default class Money extends Vue {
   display: flex;
   flex-direction: column-reverse;
 }
+
 .notes {
   padding: 12px 0;
 }
